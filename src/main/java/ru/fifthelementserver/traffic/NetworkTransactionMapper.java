a267496d2adf0264771a481fecdbdd6534e9b087
smartoven <yarik.panteleev@gmail.com>
2023-04-16 13:22:40 +0300

    поправил маппинг timestamp

--- src/main/java/ru/fifthelementserver/traffic/NetworkTransactionMapper.java
+++ src/main/java/ru/fifthelementserver/traffic/NetworkTransactionMapper.java
@@ -10,12 +10,15 @@ public class NetworkTransactionMapper {
     public NetworkTransaction fromDto(
             NetworkTransactionDto transactionDto, DeviceInfo deviceInfo
     ) {
+        Timestamp responseTimestamp = transactionDto.getResponse_date() == null
+                ? Timestamp.from(Instant.now())
+                : new Timestamp(transactionDto.getResponse_date());
         return new NetworkTransaction(
                 null,
                 deviceInfo.getId(),
                 TransactionType.fromString(transactionDto.getTransaction_type()),
-                transactionDto.getRequest_date() == null ? Timestamp.from(Instant.now()) : new Timestamp(transactionDto.getRequest_date()),
-                transactionDto.getResponse_date() == null ? Timestamp.from(Instant.now()) : new Timestamp(transactionDto.getResponse_date()),
+                transactionDto.getRequest_date() == null ? responseTimestamp : new Timestamp(transactionDto.getRequest_date()),
+                responseTimestamp,
                 transactionDto.getDuration() == null ? 0 : transactionDto.getDuration(),
                 transactionDto.getRequest_size() == null ? 0 : transactionDto.getRequest_size(),
                 transactionDto.getResponse_size() == null ? 0 : transactionDto.getResponse_size(),
